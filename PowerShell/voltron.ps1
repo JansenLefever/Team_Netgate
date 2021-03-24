c8dd75f9b1212e5dec84ef841117f5b68071ff66
ewhd <ewhd22@gmail.com>
2021-03-24 00:06:18 -0700

    Most scripts integrated. DNS+name+IP+forest has not been tested, but I'm not worried about it (it just got reformated a little, but nothing significant). Shifted a few things around in Cody's portion (for importing users) but mostly been re-writing part of his Import-Users function to handle the data we'll need and then ran into trouble from the Find-CSV function while trying to pull a csv out og google sheets. Mostly fixed it by manually downloading a .csv and working from that.

---
+++ PowerShell/voltron.ps1
@@ -0,0 +1,121 @@
+# Author: Jansen Levefer, Ethan Denny, Cody Wishart
+# Date 3/23/21
+# Project: NetGains
+
+# Attribution is done in comments at the start of sections
+
+
+# Define Variables:
+
+# $adapter = (Get-NetAdapter).ifIndex
+
+$ServerName = "S2W-Server"
+
+$ServerIPParams = @{
+  ip              = "10.10.1.5"
+  prefix          = "24"
+  GW              = "10.10.1.1"
+  InterfaceIndex  = (Get-NetAdapter).ifIndex
+}
+
+
+# These two variables contain the configuration settings for setting up a domain and a forest
+$safeModePw = ConvertTo-SecureString -String 'p@$$w0rd10' -AsPlainText -Force
+
+$forestParams = @{
+  DomainName                      = "suntowater.globexpower.com"
+  DomainNetbiosName               = "suntowater"
+  InstallDns                      = $true
+  SafeModeAdministratorPassword   = $safeModePw
+}
+
+
+
+# Define Functions:
+
+# Installs and Sets DNS
+# From Jansen
+function Setup-DNS{
+    Install-WindowsFeature DNS -IncludeManagementTools
+    Set-DnsClientServerAddress -InterfaceIndex (Get-NetAdapter).ifIndex -ServerAddresses ("LocalHost")
+    Register-DnsClient
+  }
+
+
+# Imports a CSV of users
+# From Cody
+Function Find-CSV () {
+  $check = Read-Host -Prompt 'Is the CSV file downloaded on this computer? y | n'
+  if ( $check -ieq "y" ) {
+      $dir = Read-Host -Prompt "Enter the CSV's directory (SHIFT-right click a file and click 'Copy as Path' and paste it here)"
+      return $dir
+  } 
+  elseif ( $check -ieq "n") {
+      $url = Read-Host -Prompt "Enter the shareable link for the spreadsheet (Make sure the file is public first)"
+      $url = ($url).TrimEnd("edit?usp=sharing")
+      $url = $url + "export?format=csv"
+      Invoke-WebRequest $url -OutFile "C:\Users\Administrator\Documents\temp.csv"
+      return "C:\Users\Administrator\Documents\temp.csv" 
+  }
+}
+
+Function Import-Users ($PathToCSV) {
+  $csv = Import-Csv -Path $PathToCSV
+  Write-Host $csv
+  # for($i = 0; $i -lt $csv.length; $i++){
+  #     New-ADUser -Name $csv[$i].Name -Office $csv[$i].Office -OtherAttributes @{ 'title'=$csv[$i].Title;
+  #                                                                                'mail'=$csv[$i].Email;
+  #                                                                                'company'=$csv[$i].Company;
+  #                                                                                'department'=$csv[$i].Department
+  #                                                                                }
+  foreach($user in $csv) {
+    # Format user details:
+    $Username = ("$($user.FirstName).$($user.LastName)").Replace(" ", "")
+    $UserDetails = @{
+      Name                    = "$($user.FirstName) $($user.LastName)"
+      GivenName               = $user.FirstName
+      Surname                 = $user.LastName
+      UserPrincipalName       = $Username
+      EmailAddress            = "$Username@globexpower.com"
+      Title                   = $user.title
+      Department              = $user.Department
+      Company                 = $user.Company
+      # Path                    = $null
+      ChangePasswordAtLogon   = $true
+      Enabled                 = $true
+    }
+    
+    # Create user
+    Write-Host @UserDetails
+    # New-ADUser @UserDetails -WhatIf
+  }
+}
+
+
+# Main:
+
+#### This section covers Jansen's script
+# Renames Computer
+# Rename-Computer -NewName @ServerName 
+
+# Sets a static IP
+# New-NetIPAddress @ServerIPParams
+
+# Run the Setup-DNS function to install, set, and register DNS
+# Setup-DNS
+
+
+#### This section covers Ethan's scripts
+# This line adds DNS, including management tools
+# Add-WindowsFeature ad-domain-services -IncludeManagementTools
+# Install-ADDSForest @forestParams
+
+
+#### This section covers Cody's script
+# This uses Find-CSV to get the location of the CSV from the user and format it, then stores that location as a variable and then calls Import-Users with that path as an argument
+$directory = Find-CSV
+Import-Users($directory)
+
+# Restart-Computer -Force 
+
+# End
