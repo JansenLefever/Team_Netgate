4aa91a3daeda96a68dda60d0d76d3dfc4093a358
Cody Wishart <cody.wishart@gmail.com>
2021-03-23 22:52:37 -0500

    Completed

--- PowerShell/ImportUsers.ps1
+++ PowerShell/ImportUsers.ps1
@@ -2,29 +2,37 @@
     Script Name:                  ImportUsers 
     Class Name:                   Ops 301
     Author Name:                  Cody Wishart
-    Date of latest revision:      3/22/21
-    Purpose:                      Import users into a domain
+    Date of latest revision:      3/23/21
+    Purpose:                      Import users into a domain from a csv
 #>
 
-
-# Variable dictionary 
-$userParams = @{
-    name         = "Franz Ferdinand"
-    office       = "Springfield, OR"
-    title        = "TPS Reporting Lead"
-    mail         = "ferdi@GlobeXpower.com"
-    company      = "GlobeX USA"
-    department   = "TPS"
+# Functions
+Function Find-CSV () {
+    $check = Read-Host -Prompt 'Is the CSV file downloaded on this computer? y | n'
+    if ( $check -ieq "y" ) {
+        $dir = Read-Host -Prompt "Enter the CSV's directory (SHIFT-right click a file and click 'Copy as Path' and paste it here)"
+        return $dir
+    } 
+    elseif ( $check -ieq "n") {
+        $url = Read-Host -Prompt "Enter the shareable link for the spreadsheet (Make sure the file is public first)"
+        $url = ($url).TrimEnd("edit?usp=sharing")
+        $url = $url + "export?format=csv"
+        Invoke-WebRequest $url -OutFile "C:\Users\Administrator\Documents\temp.csv"
+        return "C:\Users\Administrator\Documents\temp.csv" 
+    }
 }
 
-# Function to add specified user from dictionary 
-Function importUsers($name, $office, $title, $mail, $company, $department) {
-    New-ADUser -Name $name -Office $office -OtherAttributes @{ 'title'=$title;
-                                                               'mail'=$mail;
-                                                               'company'=$company;
-                                                               'department'=$department
-                                                                }
-}
+Function Import-Users () {
+    $directory = Find-CSV
+    $csv = Import-Csv -Path $directory
+    for($i = 0; $i -lt $csv.length; $i++){
+        New-ADUser -Name $csv[$i].Name -Office $csv[$i].Office -OtherAttributes @{ 'title'=$csv[$i].Title;
+                                                                                   'mail'=$csv[$i].Email;
+                                                                                   'company'=$csv[$i].Company;
+                                                                                   'department'=$csv[$i].Department
+                                                                                   }
+    }
+} 
 
 # Main
-importUsers @userParams
+Import-Users 
