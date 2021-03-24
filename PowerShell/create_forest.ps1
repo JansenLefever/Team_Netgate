5dcee71b2d5c471e4f0447a214c90baf887c96c0
ewhd <ewhd22@gmail.com>
2021-03-23 21:25:00 -0700

    changed domain name from globex.com to globexpower.com

--- PowerShell/create_forest.ps1
+++ PowerShell/create_forest.ps1
@@ -1,6 +1,6 @@
 # Script:                       create_forest.ps1
 # Author:                       Ethan Denny
-# Date of latest revision:      3/22/2021
+# Date of latest revision:      3/23/2021
 # Purpose:     This script will add a new forest in AD DS
 
 # Walked through by this video: https://www.youtube.com/watch?v=bWF1-rhPh5E&ab_channel=TechSnips
@@ -14,7 +14,7 @@ Add-WindowsFeature ad-domain-services -IncludeManagementTools
 $safeModePw = ConvertTo-SecureString -String 'p@$$w0rd10' -AsPlainText -Force
 
 $forestParams = @{
-    DomainName                      = "suntowater.globex.com"
+    DomainName                      = "suntowater.globexpower.com"
     DomainNetbiosName               = "suntowater"
     InstallDns                      = $true
     SafeModeAdministratorPassword   = $safeModePw
